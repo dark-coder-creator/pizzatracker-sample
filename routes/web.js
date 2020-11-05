fd75b97253e6d863b6414e14b1630161a7257064
Cheloor Saravanan Mohandas <saravanan.mohandas24@gmail.com>
2020-11-05 18:23:36 +0530

    orders created

--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,10 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController=require('../app/http/controllers/authController')
 const cartController=require('../app/http/controllers/customers/cartController')
+const orderController=require('../app/http/controllers/customers/orderController')
 const guest=require('../app/http/middlewares/guest')
+const auth=require('../app/http/middlewares/auth')
+const AdminOrderController=require('../app/http/controllers/admin/orderController')
 function initRoutes(app)
 {
  
@@ -17,6 +20,15 @@ function initRoutes(app)
 
     app.get('/cart',cartController().index)
     app.post('/update-cart',cartController().update)
+
+    
+    //customer routes
+    app.post('/orders',auth,orderController().store)
+    app.get('/customer/orders',auth,orderController().index)
+
+    //admin routes
+    app.get('/admin/orders',auth,AdminOrderController().index)
+
 }
 
 module.exports=initRoutes
